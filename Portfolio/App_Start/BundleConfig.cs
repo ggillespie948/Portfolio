216b914f50d31b73f0426f2895e83d22340573ec
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-11-24 22:57:18 +0000

    Initial setup prototype

--- Portfolio/App_Start/BundleConfig.cs
+++ Portfolio/App_Start/BundleConfig.cs
@@ -11,8 +11,21 @@ namespace Portfolio
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/siteJS").Include(
+                        "~/Scripts/Site.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+
+            // animate bundles
+            bundles.Add(new StyleBundle("~/bundles/animate").Include(
+                "~/Content/animate.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/wowJs").Include(
+                        "~/Scripts/wow.js"));
+
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
